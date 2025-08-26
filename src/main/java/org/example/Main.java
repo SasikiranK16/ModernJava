205c1d5396c83cb968aff86ddfac549713c581d1
BunnySasi <sasikiran2213@gmail.com>
2025-08-26 13:48:22 +0530

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,14 +9,23 @@ public class Main {
     public static final Logger LOGGER = Logger.getLogger(Main.class.getName());
     /* Canonical Constructor */
     record Human(String fname){
-        Human(String fname){
-            Objects.requireNonNull(fname, "fname is null");
-            this.fname = fname.toUpperCase();
+
+//        Human(String fname){
+//            Objects.requireNonNull(fname, "fname is null");
+//            this.fname = fname.toUpperCase();
+//        }
+        /* Compact Constructor */
+        Human{
+            Objects.requireNonNull(fname,"First Name Should not be null");
+            if (fname.length()<3){
+                throw new IllegalArgumentException("First Name should have at least 3 characters");
+            }
+            fname = fname.toUpperCase();
         }
     }
     public static void main(String[] args) {
-        var person = new Human("Hellos");
+        var person = new Human("hellos");
         LOGGER.info("Human name: " + person.fname());
     }
-    Hello
+
 }
